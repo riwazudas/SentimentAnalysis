8fdfac8dbd9b5a10f9ccd7f2ae3e3562e0f0f7ea
riwaz <riwazudas@gmail.com>
2025-06-14 18:09:59 +1000

    port fix

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def home():
     return {"message": "Sentiment Analysis API is up and running!"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
