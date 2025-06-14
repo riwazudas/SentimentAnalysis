7bcb71443d7ff7a4d6545ecfd832edc0f1803964
riwaz <riwazudas@gmail.com>
2025-06-14 17:41:45 +1000

    log file check

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from apis.routes import api_blueprint 
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(api_blueprint, url_prefix='/api')
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 # Setup logging
 handler = RotatingFileHandler('logs/app.log', maxBytes=10000, backupCount=3)
